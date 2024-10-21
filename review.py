24a09f86f2b6073d4a9f4378aab1c9dafb38ccef
Lega3 <136846231+lmadisha@users.noreply.github.com>
2024-10-21 15:21:42 +0200

    Added new functions to the review.py get_reviews and edit_review.
    Signed-off-by: Legae Madisha <lmadisha023@student.wethinkcode.co.za>

--- review.py
+++ review.py
@@ -1,5 +1,8 @@
 import os
+import pprint
 from subprocess import PIPE, Popen
+
+from review_maker import edited_reviews
 
 
 def cmdline(command):
@@ -32,3 +35,32 @@ class Review:
         self.projects = []
         self.path = os.path.expanduser("~") + f'/Desktop/work/review/{self.user_input_review}'
 
+    def get_reviews(self):
+        """
+        These function gets the raw data from the command line from the WTC lms software.
+        :return: The newly edited and fixed reviews
+        """
+        self.reviews = cmdline("wtc-lms reviews").splitlines()
+
+        for n in self.reviews:
+            if "Please login using" in n.decode("utf-8"):
+                print("First login into the terminal.")
+                exit(0)
+
+        self.reviews = cmdline("wtc-lms reviews").splitlines()
+        return edited_reviews(self.reviews)
+
+    def edit_reviews(self, raw_reviews: list):
+        """
+        This function edits the raw review data by the status of the review/activity.
+        :param raw_reviews: The list of raw reviews data.
+        :return: The list of the edited reviews from the raw reviews data.
+        """
+        for review in raw_reviews:
+            for status in self.statuses:
+                if status in review.decode("utf-8"):
+                    self.edited_reviews.append(review.decode("utf-8"))
+
+        return self.edited_reviews
+    
+    
